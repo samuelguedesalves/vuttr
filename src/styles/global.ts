ba7b3c55097976af7a9a6438ab95311dfe26fc59
samuel guedes <guedes.works7@gmail.com>
2022-04-09 03:23:32 -0400

    feat: modify modal styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,4 @@
+import { transparentize } from 'polished';
 import { globalCss } from './stitches.config';
 
 export const globalStyles = globalCss({
@@ -11,4 +12,70 @@ export const globalStyles = globalCss({
     margin: 0,
     padding: 0,
   },
+
+  body: {
+    background: '$white',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: '$text',
+    fontWeight: 'normal',
+  },
+
+  'h1, h2, h3, h4, h5, h6, strong': {
+    fontWeight: 'bold',
+  },
+
+  button: {
+    cursor: 'pointer',
+  },
+
+  '.react-modal-overlay': {
+    position: 'fixed',
+    width: '100%',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+
+    background: transparentize(0.6, '#170C3A'),
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  '.react-modal-content': {
+    width: '100%',
+    maxWidth: 600,
+    background: '$white',
+
+    padding: 30,
+    borderRadius: 6,
+
+    position: 'relative',
+  },
+
+  '.react-modal-content button[type="button"]': {
+    position: 'absolute',
+    width: 14,
+    height: 14,
+    top: 30,
+    right: 30,
+
+    background: 'transparent',
+    border: 'transparent',
+
+    transition: 'filter 0.2s',
+
+    img: {
+      width: 14,
+      height: 14,
+    },
+
+    '&:hover': {
+      filter: 'brightness(0.9)',
+    },
+  },
+
 });
