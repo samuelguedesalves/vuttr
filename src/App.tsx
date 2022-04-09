59544934eeb2d8ca594e2243b8a21dab6c70696b
samuel guedes <guedes.works7@gmail.com>
2022-04-09 03:29:53 -0400

    feat: modify 'header' and implement things into application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { atom } from 'jotai';
+
+import { AddNewToolModal } from './components/organisms/AddNewToolModal';
 import { Header } from './components/organisms/Header';
+import { ItemsList } from './components/organisms/ItemsList';
 import { globalStyles } from './styles/global';
+import { ToolsContextProvider } from './hooks/tools';
 
 const App: React.FC = () => {
+  const isOpenAddNewToolModalAtom = atom<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [searchInTagsOnly, setSearchInTagsOnly] = useState<boolean>(false);
+
   globalStyles();
 
   return (
     <div className="App">
-      <Header />
+      <ToolsContextProvider>
+        <Header
+          setSearchQuery={setSearchQuery}
+          isOpenAddNewToolModalAtom={isOpenAddNewToolModalAtom}
+          setSearchInTagsOnly={setSearchInTagsOnly}
+        />
+        <ItemsList />
+
+        <AddNewToolModal
+          isOpenAddNewToolModalAtom={isOpenAddNewToolModalAtom}
+        />
+      </ToolsContextProvider>
     </div>
   );
 };
